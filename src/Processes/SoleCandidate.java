92ddd92c12f3130f214472e5a8ad818dbe166e30
Sahil Mehta <sahilmehta17@gmail.com>
2017-05-25 14:28:59 -0500

    realized that I was WAY overcomplicating things. There was no need to even care about rows, columns, or blocks (that's important for unique candidate). Fixed sole candidate to use a very simple method to check all spots for potential update. A Plus: I got a BlockPuzzle Iterator and a Block toString out of it :).

--- src/Processes/SoleCandidate.java
+++ src/Processes/SoleCandidate.java
@@ -17,12 +17,12 @@ import java.util.Iterator;
 
 public class SoleCandidate extends AbstractProcess {
 
-    public SoleCandidate(Square[][] standard_board,
+    public SoleCandidate(Square[][] standard_puzzle,
                          AbstractVectorPuzzle row_puzzle,
                          AbstractVectorPuzzle col_puzzle,
                          BlockPuzzle block_puzzle)
     {
-        super(standard_board, row_puzzle, col_puzzle, block_puzzle);
+        super(standard_puzzle, row_puzzle, col_puzzle, block_puzzle);
     }
 
     /**
@@ -33,75 +33,27 @@ public class SoleCandidate extends AbstractProcess {
     public void execute()
     {
         boolean execute_loop_trigger = true;
+        
         while (execute_loop_trigger)
-        {
-            boolean row_change = vector_sole_candidate(row_puzzle);
-            boolean col_change = vector_sole_candidate(col_puzzle);
-            boolean block_change = block_sole_candidate(block_puzzle);
-
-            execute_loop_trigger = row_change || col_change || block_change;
-        }
+            execute_loop_trigger = standard_sole_candidate();
     }
 
-    /**
-     * @param puzzle AbstractVectorPuzzle to check for sole candidates
-     * @return true if a change was made, false otherwise
-     */
-    public boolean vector_sole_candidate(AbstractVectorPuzzle puzzle)
+    public boolean standard_sole_candidate()
     {
-        Iterator<Vector> vector_iter;
-        Iterator<Square> square_iter;
         boolean change_made = false;
 
-        /**
-         * Iterate through all Vectors in puzzle
-         * check all Squares in each Vector for sole candidates
-         * update values if there was a sole candidate
-         */
-        vector_iter = puzzle.iterator();
-        while (vector_iter.hasNext())
+        for (int r = 0; r < standard_puzzle.length; r++)
         {
-            square_iter = vector_iter.next().iterator();
-            while (square_iter.hasNext())
+            for (int c = 0; c < standard_puzzle[r].length; c++)
             {
-                Square current = square_iter.next();
+                Square current = standard_puzzle[r][c];
                 int current_row = current.get_row();
                 int current_col = current.get_col();
 
                 if (current.get_candidates().size() == 1)
                 {
                     set_value(current_row, current_col, current.get_final_candidate());
-                }
-            }
-        }
-
-        return change_made;
-    }
-
-    public boolean block_sole_candidate(BlockPuzzle puzzle)
-    {
-        Iterator<Block> block_iter;
-        Iterator<Square> square_iter;
-        boolean change_made = false;
-
-        /**
-         * Iterate through all Vectors in puzzle
-         * check all Squares in each Vector for sole candidates
-         * update values if there was a sole candidate
-         */
-        block_iter = puzzle.iterator();
-        while (block_iter.hasNext())
-        {
-            square_iter = block_iter.next().iterator();
-            while (square_iter.hasNext())
-            {
-                Square current = square_iter.next();
-                int current_row = current.get_row();
-                int current_col = current.get_col();
-
-                if (current.get_candidates().size() == 1)
-                {
-                    set_value(current_row, current_col, current.get_final_candidate());
+                    change_made = true;
                 }
             }
         }
